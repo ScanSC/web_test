abd2273725527b17fa3406d834c731eabcb404bf
Scan <stanislas.commenge@epitech.eu>
2022-07-26 21:17:54 +0200

    [ADD] centered elements and fixed bop

--- test.js
+++ test.js
@@ -1,16 +1,3 @@
-class Game {
-    constructor(name, type, price) {
-        this.name = name;
-        this.type = type;
-        this.price = price;
-    }
-}
-
-let games = [];
-games.push(new Game("Rainbow Six : Siege", "fps", 7));
-games.push(new Game("Stray", "enigme, aventure", 25));
-games.push(new Game("No Man Sky", "aventure, exploration", 30));
-
 function switch_bop(img_id) {
     let image = document.getElementById(img_id);
     if (image.getAttribute('src') === 'images/bop_open.png') {
